619cfaa4fb6132c6ff4e1b7f7168a4196d6dc535
codewithsrobins1 <codewithsrobins1@gmail.com>
2025-03-23 12:06:54 -0700

    start gsap animations

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface ButtonProps {
+  id?: string;
+  title?: any;
+  leftIcon?: any;
+  rightIcon?: any;
+  containerClass?: any;
+}
+
+const Button = ({
+  id,
+  title,
+  leftIcon,
+  rightIcon,
+  containerClass,
+}: ButtonProps) => {
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+        <div>{title}</div>
+      </span>
+    </button>
+  );
+};
+
+export default Button;
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,9 @@
 "use client";
 import React, { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -7,13 +11,68 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
   const totalVideos = 4;
-  const nextVideoRef = useRef(null);
+  const nextVideoRef = useRef<HTMLVideoElement | null>(null);
+  const upcomingVidIndex = (currentIndex % totalVideos) + 1; // Don't go past the number of videos available
 
   const handleMiniVdClick = () => {
     setHasClicked(true);
 
-    setCurrentIndex((prevIdx) => prevIdx + 1);
+    setCurrentIndex(upcomingVidIndex);
   };
+
+  // GSAP ANIMATION - Video Player Expansion
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        // Set the visibility of the next video element to "visible"
+        gsap.set("#next-video", { visibility: "visible" });
+
+        // Animate the transition for the next video
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => {
+            if (nextVideoRef.current) {
+              nextVideoRef.current.play();
+            }
+          },
+        });
+
+        // Animate the current video as it shrinks out of view
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    { dependencies: [currentIndex], revertOnUpdate: true }
+  );
+
+  // GSAP ANIMATION - Move Hero Video Player
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%",
+      borderRadius: "0 0 40% 10%",
+    });
+
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  }, []);
 
   const getVideoSource = (index: any) => `videos/hero-${index}.mp4`;
 
@@ -35,7 +94,7 @@ const Hero = () => {
             >
               <video
                 ref={nextVideoRef}
-                src={getVideoSource(currentIndex + 1)}
+                src={getVideoSource(upcomingVidIndex)}
                 loop
                 muted
                 id="current-video"
@@ -44,8 +103,55 @@ const Hero = () => {
               />
             </div>
           </div>
+
+          {/* Hidden Video that gets animated with zoom-in effect */}
+          <video
+            ref={nextVideoRef}
+            src={getVideoSource(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          />
+
+          <video
+            src={getVideoSource(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+
+        <h1 className="special-font-b hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>
+              <b>e</b>
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
+            />
+          </div>
         </div>
       </div>
+
+      <h1 className="special-font-b hero-heading absolute bottom-5 right-5 text-black">
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };
